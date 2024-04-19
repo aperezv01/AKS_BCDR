7cea12c22502e8e7e8553b4da947ad68590b040d
aperezv01 <83298224+aperezv01@users.noreply.github.com>
2024-04-19 09:35:21 -0500

    Update psfunc.ps1

--- csu_dbresiliency/function/psfunc.ps1
+++ csu_dbresiliency/function/psfunc.ps1
@@ -1,2 +1,3 @@
 Register-AzResourceProvider -ProviderNamespace Microsoft.Cdn
+New-AzureRmResourceGroup -Name rgdbresiliency -Location "east us"
 New-AzResourceGroupDeployment -ResourceGroupName rgdbresiliency -TemplateFile .\functions_fd.bicep -appInsightsLocation "east us"
